3c947cbfcc439e6792fd82878632ba951f5a8228
현준 <hyeonjun0125@naver.com>
2022-01-06 19:53:35 +0900

    ㅇ

--- C/main.c
+++ C/main.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-struct AA function(int j);
-struct AA
+int Print_Status(struct HUMAN human);
+struct HUMAN
 {
-    int i;
+    int age;
+    int height;
+    int weight;
+    int gender;
 };
 int main()
 {
-    struct AA a;
-    a=function(10);
-    printf("a.i : %d \n",a.i);
-    return 0;
+    struct HUMAN Adam = {31, 182, 75, 0};
+    struct HUMAN Eve = {27, 166, 48, 1};
+    Print_Status(Adam);
+    Print_Status(Eve);
 }
-struct AA function(int j)
+int Print_Status(struct HUMAN human)
 {
-    struct AA A;
-    A.i=j;
-    return  A;
+    if(human.gender == 0)
+        printf("MAN\n");
+    else
+        printf("FEMALE\n");
+    printf("AGE : %d / Height : %d / Weight : %d \n", human.age, human.height, human.weight);
+    if(human.gender == 0 && human.height>=180)
+        printf("HE IS A WINER!!! \n");
+    else
+        printf("")
 }
